0eb7d2b0a06f3e43735ad60f1611c4597188c3ec
MLFairy <mlfairy@vijaysharma.ca>
2021-06-30 20:52:29 -0400

    Fixed SPM

--- Package.swift
+++ Package.swift
@@ -15,13 +15,12 @@ let package = Package(
             url: "https://github.com/krzyzanowskim/CryptoSwift.git",
             .exact("1.4.0")
         )
-    ]
+    ],
     targets: [
         .binaryTarget(
             name: "MLFSupport",
             url: "https://firebasestorage.googleapis.com/v0/b/mlfairy.appspot.com/o/sdk%2FMLFSupport-0.0.10.xcframework.zip?alt=media",
-            checksum: "e8cc31ae801d90801689dfb714b352bec53ea3ce628fcae9a785c6abd3f9550d",
-            dependencies: ["CryptoSwift"]
-        ),
+            checksum: "e8cc31ae801d90801689dfb714b352bec53ea3ce628fcae9a785c6abd3f9550d"
+        )
     ]
 )
